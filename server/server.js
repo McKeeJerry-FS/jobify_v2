ff3473b42e5f4ceb035595e34e21f784f9c053ff
jerry mckee <jmckeejr@safehomesecurityinc.com>
2026-03-05 10:35:04 -0500

    setting up server

--- server/server.js
+++ server/server.js
@@ -1,3 +1,38 @@
-import { value } from './test.js';
+import express from 'express';
+import morgan from 'morgan';
+import * as dotenv from 'dotenv';
 
-console.log(value);
+dotenv.config();
+
+const app = express();
+
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'));
+}
+
+app.get('/', (req, res) => {
+  res.send('Hello React!');
+});
+
+app.use(express.json());
+
+app.post('/',(req,res) => {
+  console.log(req);
+  res.json({message: 'Data received successfully', data: req.body});
+});
+
+try {
+  const response = await fetch(
+    'https://www.course-api.com/react-useReducer-cart-project'
+  );
+  const cartData = await response.json();
+  console.log(cartData);
+} catch (error) {
+  console.log(error);
+}
+
+const port = process.env.PORT || 5100;  
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
